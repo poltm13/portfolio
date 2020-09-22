b7864af51bf9c6f119897ea75138cd13f7bd28d0
Pol <poltm13@gmail.com>
2020-09-22 02:09:34 +0200

    working on dark theme

--- src/components/ThemeToggler.js
+++ src/components/ThemeToggler.js
@@ -1,36 +1,57 @@
-import React from "react";
+import React, { useState } from "react";
+import sun from "../assets/sun.png";
+import moon from "../assets/moon.png";
 
-export default function themeToggler(props) {
+export default function ThemeToggler(props) {
+  const [darkTheme, setTheme] = useState(false);
+
   const toggler = {
     width: "45px",
     height: "22px",
-    background: "grey",
+    background: "#7af2ff",
     right: "0",
     top: "50%",
     borderRadius: "20px",
-    transform: "translate(-50%, -50%)",
+    transform: "translate(-100%, -50%)",
+  };
+
+  const togglerDark = {
+    background: "#4e4e4e",
   };
 
   const toggle = {
     content: "''",
-    background: "white",
+    background: "url(" + sun + ") 0% 0% / 17px",
     width: "17px",
     height: "17px",
     position: "absolute",
     borderRadius: "50%",
     top: "0",
     transform: "translate(2.5px, 2.5px)",
-    transition: "all .3s ease-in-out",
+    transition: "all .3s",
     left: "0",
+  };
+
+  const toggleDark = {
+    background: "url(" + moon + ") 0% 0% / 17px",
+    transform: "translate(25.5px, 2.5px)",
   };
 
   const toggleHandler = () => {
     document.querySelector("body").classList.toggle("dark-mode");
+    setTheme(!darkTheme);
   };
 
   return (
-    <div style={toggler} className={props.className} onClick={toggleHandler}>
-      <div style={toggle}></div>
+    <div
+      style={darkTheme ? { ...toggler, ...togglerDark } : toggler}
+      className={props.className}
+      onClick={toggleHandler}
+    >
+      <div
+        style={darkTheme ? { ...toggle, ...toggleDark } : toggle}
+        className="switch"
+      ></div>
     </div>
   );
 }
